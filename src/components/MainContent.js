fa6c8ba4e01d9ecad6becea87395a1ecea891c2b
Jeff Hill <jeffhill1978@gmail.com>
2019-12-19 16:31:59 -0600

    set up new react app with components

---
+++ src/components/MainContent.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function MainContent() {
+    return (
+        <main>
+            <h1>This is the Main Content</h1>
+        </main>
+    )
+}
+export default MainContent
---
+++ src/components/MyInfo.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+    <div>
+        <hi>Jeff</hi>
+        <p>This is a practice exercise</p>
+         <ul>
+             <li>Favorite Vacay Spots</li>
+             <ul>
+             <li>Practice React</li>
+             <li>inserting unordered list</li>
+             <li>using JSx</li>
+             </ul>
+         </ul>
+    </div>
+    )
+ }
+
+ export default MyInfo
